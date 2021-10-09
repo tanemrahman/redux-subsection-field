bd83c2167ef22b247cf5387e19ad517c2edd186b
Tanem Rahman <53551897+tanemrahman@users.noreply.github.com>
2021-10-09 23:46:12 +0600

    Create theme-options.php
    The code of the subsection field needs to be added.

---
+++ theme-options.php
@@ -0,0 +1,6 @@
+// Start New Subsection title.
+array(
+    'id'            => 'subsection-footer-copyright-start',
+    'subtitle'      => esc_html__( 'Copyrights', 'reflex' ),
+    'type'          => 'subsection',
+),
